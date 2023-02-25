02a97926008430b6967adf3c7b0eed053bf2b85d
Mete <109483424+meteulken@users.noreply.github.com>
2023-02-25 19:20:00 +0300

    Update exit_game.c

--- So_Long/sources/exit_game.c
+++ So_Long/sources/exit_game.c
@@ -14,12 +14,23 @@
 
 void	eventsuccess(t_game *game)
 {
-	game->intra_str = "mulken";
+	char *intra_str = "mulken";
+    ft_printf("\033[1;31m");
+    ft_printf(" ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    ft_printf(" ┃\033[1;35m       THANK YOU FOR       \033[1;31m┃\n");
+    ft_printf(" ┃\033[1;35m           PLAYING        \033[1;31m┃\n");
+    ft_printf(" ┃\033[1;35m        THIS AWESOME      \033[1;31m┃\n");
+    ft_printf(" ┃\033[1;35m            GAME!         \033[1;31m┃\n");
+    ft_printf(" ┃\033[1;35m     INTRA: \033[1;33m%-10s\033[1;35m   \033[1;31m┃\n", intra_str);
+    ft_printf(" ┃                         ┃\n");
+    ft_printf(" ┃\033[1;35m      Press \033[1;33m[Q]\033[1;35m to exit     \033[1;31m┃\n");
+    ft_printf(" ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛\n\n");
+	/*game->intra_str = "mulken";
 	ft_printf("\n\n------------------------\n\n");
 	ft_printf(MAGENTA" THANK YOU FOR PLAYING \n\n"RESET);
 	ft_printf(YELLOW"    INTRA: %s \n"RESET, game->intra_str);
 	ft_printf(" Press [Q] to exit \n\n");
-	ft_printf("------------------------\n");
+	ft_printf("------------------------\n");*/
 }
 
 void	free_map(char **map)
